97b4d9983a4eb3e51a3218ae5a14c135e24729f3
hillna <nick@nickh.info>
2022-12-07 11:48:40 -0500

    Updated day 3 part 1 2022

---
+++ 2022/3/part1-1.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+const priorities = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func main() {
+
+	input, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer input.Close()
+	scanner := bufio.NewScanner(input)
+
+	total := 0
+	for scanner.Scan() {
+		text := scanner.Text()
+		index := len(text) / 2
+
+		container1 := text[:index]
+		container2 := text[index:]
+
+		for x := 0; x < len(container1); x++ {
+			item := string(container1[x])
+
+			if strings.Index(container2, item) > -1 {
+				total = total + strings.Index(priorities, item) + 1
+				break
+			}
+		}
+	}
+
+	fmt.Println("Total:", total)
+}
